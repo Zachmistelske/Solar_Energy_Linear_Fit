1c774ea891117fe58d4e40f5c427f7d35ca1311b
Zachmistelske <zachmistelske@gmail.com>
2022-12-01 00:45:40 -0800

    Add files via upload

--- linearfitsolar.py
+++ linearfitsolar.py
@@ -17,51 +17,64 @@ h.hist(bins=100, figsize=(15, 10))
 plt.show()
 
 c=h['Year']/h['Avg_Solar']
-u=h['Year']
+v=h['Year']
 r=h['Avg_Solar']
-t=r.astype(float)
-v=np.zeros(len(t))
-for i in range(len(t)):
-    v[i]=np.log(t[i])
+u=r.astype(float)
 q1=h['StdDev']
-q2=q1.astype(float)
-q=np.zeros(len(q1))
-for i in range(len(q1)):
-    q[i]=math.log(q2[i])
-# make a linear point plot of data set displaying errors to each y-value 
-plt.scatter(u,v,color='red')
-plt.errorbar(u,v,q,color='red',fmt='o',label='data set')
-# calculate constant 'a' in the linear equation y=a*x that best represents data first by breaking up formula's sums into arrays for each data value
-# us=x^2/sigma^2
-us=np.zeros(len(u))
-for i in range(len(u)):
-    us[i]=((u[i])**2)/((q[i])**2)
+q=q1.astype(float)
+# make a linear point plot of data set displaying errors to each y-value
+plt.scatter(u,v,color='green')
+plt.errorbar(u,v,q,color='green',fmt='o',label='data set')
+# calculate constant 'a' and 'b' in the linear equation y=a+b*x that best represents data first by breaking up formula's sums into arrays for each data value
 # uv=x*y/sigma^2
-uv=np.zeros(len(u))
+xy=np.zeros(len(u))
 for i in range(len(u)):
-    uv[i]=((u[i])*(v[i]))/((q[i])**2)
-# divide sums to obtain a
-a=(sum(uv))/(sum(us))
-# multiply each data value x by calculated a to obtain yfit values and plot linear function
+    xy[i]=(u[i]*v[i])/((q[i])**2)
+# us=x^2/sigma^2
+xs=np.zeros(len(u))
+for i in range(len(u)):
+    xs[i]=((u[i])**2)/((q[i])**2)
+# nu=x/sigma^2
+nx=np.zeros(len(u))
+for i in range(len(u)):
+    nx[i]=(u[i])/((q[i])**2)
+# nv=y/sigma^2
+ny=np.zeros(len(v))
+for i in range(len(v)):
+    ny[i]=(v[i])/((q[i])**2)
+# j=1/sigma^2
+j=np.zeros(len(q))
+for i in range(len(q)):
+    j[i]=(1/((q[i])**2))
+# calculate determinant of inverse matrix that is used to find a and b
+det=(sum(j)*sum(xs))-((sum(nx)))**2
+# calculate a by multiplying and subtracting sums then divide by determinant
+a=((sum(ny)*sum(xs))-(sum(nx)*sum(xy)))/(det)
+# calculate b by multiplying and subtracting sums then divide by determinant
+b=((sum(j)*sum(xy))-(sum(nx)*sum(ny)))/(det)
+# calculate errors in determining a and b
+errora=math.sqrt((sum(xs))/(det))
+errorb=math.sqrt((sum(j))/(det))
+# calculate yfit values by plugging calculated values a and b into yfit equation yfit=a+b*x
 yfit=np.zeros(len(u))
 for i in range(len(u)):
-    yfit[i]=a*u[i]
-plt.plot(u,yfit,color='green',label='fitted linear function')
-# calculate chi-squared to evaluate between data and yfit
+    yfit[i]=a+b*(u[i])
+# plot yfit function 
+plt.plot(u,yfit,color='purple',label='fitted linear function')
+# calculate chi-squareed to test agreement between yfit and data
 chi=np.zeros(len(v))
 for i in range(len(v)):
     chi[i]=((yfit[i]-v[i])/(q[i]))**2
 chis=sum(chi)
-# calculate the error in determining the constant a
-ae=np.zeros(len(u))
-for i in range(len(u)):
-    ae[i]=(((u[i])**2)/((q[i])**2))
-aerror=math.sqrt((1)/(sum(ae)))
-# display results
+# display calculated values a,b,their errors, and chi-squared
 print('Best-fit value of a:',a)
-print('uncertainty in a:',aerror)
+print('Best-fit value of b:',b)
+print('uncertainty in a:',errora)
+print('uncertainty in b:',errorb)
 print('chi-squared:',chis)
-plt.xlabel('Year')
-plt.ylabel('Avg_Solar_Consumption')
+
+plt.xlabel('Avg Solar Consumption (kWh)')
+plt.ylabel('Year')
 plt.legend()
 plt.show()
+
